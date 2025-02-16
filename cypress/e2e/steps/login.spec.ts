1fd8b252b4bb3dfe87429a934b738e8dc3570460
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-02-16 20:37:13 -0300

    fix test

--- cypress/e2e/steps/login.spec.ts
+++ cypress/e2e/steps/login.spec.ts
@@ -4,18 +4,18 @@ Given('que estou na página de login', () => {
   cy.visit('/login');
 });
 
-When('eu insiro {string} no campo de usuário', (username: string) => {
-  cy.get('#username').type(username);
+When('eu insiro {string} no campo de usuário', (login: string) => {
+  cy.get('[data-cy=login]').type(login);
 });
 
 When('eu insiro {string} no campo de senha', (password: string) => {
-  cy.get('#password').type(password);
+  cy.get('[data-cy=password]').type(password);
 });
 
 When('eu clico no botão de login', () => {
-  cy.get('#login-button').click();
+  cy.get('[data-cy=btn-login]').click();
 });
 
 Then('eu devo ser redirecionado para a página inicial', () => {
-  cy.url().should('include', '/home');
+  cy.url().should('include', '/shop');
 });
