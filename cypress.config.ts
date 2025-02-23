d9833a5a3d6887feac4b0ef8bb4b45a8d91f50ac
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-02-22 21:47:40 -0300

    correcao e2e shop

--- cypress.config.ts
+++ cypress.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: '**/*.feature',
+    supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents,
   },
 });
